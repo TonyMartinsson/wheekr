3d51bfc4f63181e3cc0a0bc9449d46a8ff713e1f
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-04-21 16:03:30 +0200

    split repo

--- server/server.js
+++ server/server.js
@@ -13,16 +13,19 @@ const app = express();
 
 app.use(express.json())
 // definera alla endpoints för erat API (router)
+
+// Detta skall ligga i postRouter
 app.get('/api/posts', async (req, res) => {
     const posts = await PostModel.find({});
     res.status(200).json(posts);
 });
+
 //app.use(userRouter);
 //app.use(postRouter);
 
 // Lägg till en felhanterare (VG)
 
-app.listen(4000)
+app.listen(4000);
 
 
 // npm i express
