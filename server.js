c5d2f3a014f5efc71ba93d0f18200e8b120c471a
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-21 15:45:18 +0200

    handledning

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const express = require('express')
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
@@ -7,8 +9,24 @@ db.once('open', function() {
     console.log('HEJ SVEJS')
 });
 
+const app = express();
+
+app.use(express.json())
+// definera alla endpoints för erat API (router)
+app.get('/api/posts', async (req, res) => {
+    const posts = await PostModel.find({});
+    res.status(200).json(posts);
+});
+//app.use(userRouter);
+//app.use(postRouter);
+
+// Lägg till en felhanterare (VG)
+
+app.listen(4000)
+
 
 // npm i express
 // npm i nodemon
 // npm i mongoose
 // installera servern för ditt OS.
+
