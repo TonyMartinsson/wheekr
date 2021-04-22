b4de81d9fb159605f287a386122753f10c88269c
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-22 11:48:51 +0200

    axios

--- client/src/components/NewsFeed.jsx
+++ client/src/components/NewsFeed.jsx
@@ -1,34 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container } from '@material-ui/core';
-import { makeStyles, createStyles} from '@material-ui/core/styles';
+//import { makeStyles, createStyles} from '@material-ui/core/styles';
 import UserPost from './UserPost';
 import List from '@material-ui/core/List';
 import NewPost from './NewPost';
+import axios from 'axios';
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-    item: {
-      display: 'flex',
-      justifyContent: 'center'
+
+// const useStyles = makeStyles((theme) =>
+//   createStyles({
+//     root: {
+//       flexGrow: 1,
+//     },
+//     paper: {
+//       padding: theme.spacing(2),
+//       textAlign: 'center',
+//       color: theme.palette.text.secondary,
+//     },
+//     item: {
+//       display: 'flex',
+//       justifyContent: 'center'
+//     }
+//   }),
+// );
+
+class NewsFeed extends Component { 
+    constructor(props) {
+      super(props);
+      this.state = {
+        posts: []
+      };
     }
-  }),
-);
+  
+    componentDidMount() {
+      axios
+        .get('http://localhost:3000/api/posts')
+        .then(res => {
+          this.setState({
+            posts: res.data
+          })
+        })
+        .catch(err =>{
+          console.log('Error');
+        })
+    };
+  
+  
+    render() {
+      const posts = this.state.posts;
+      console.log("PrintBook: " + posts);
+      let postList;
+  
+      if(!posts) {
+        postList = "No posts!";
+      } else {
+        postList = posts.map((post, k) =>
+          <UserPost post={post} key={k} />
+        );
+        console.log(postList);
+      } 
+    
 
-function NewsFeed() { 
-    const classes = useStyles(); 
+
     return (
-        <div className={classes.root}>
+        <div>
           <Container maxWidth="md">
             <NewPost/>
-          <List className={classes.root}>
+          <List>
+            {postList}
              <UserPost/>
              <UserPost/>
              <UserPost/>
@@ -37,4 +76,6 @@ function NewsFeed() {
         </div>
       );
     } 
+}
+
 export default NewsFeed;
