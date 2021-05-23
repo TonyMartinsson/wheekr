fc0f53af54bef41f66a5936121314ed351d71035
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-05-23 20:58:01 +0200

    fixed update post function

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -2,21 +2,13 @@ const express = require('express');
 const PostModel = require('../models/post.model');
 const router = express.Router();
 
-router.get('/api/posts', async (req, res) => {
-    const posts = await PostModel.find({});
-    res.status(200).json(posts);
-});
-
-router.get('/api/posts/adminaccess', checkAdminAccess, async (req, res) => {
-    const posts = await PostModel.find({});
-    res.status(200).json(posts);
-});
+// MIDDLEWARES
 
 function checkLogin(req, res, next) {
     if(req.session.username) {
         next()   
     } else {
-        res.status(401).json(null)
+        res.status(401).json("No user is logged in.")
     }
 }
 
@@ -29,29 +21,16 @@ function checkAdminAccess(req, res, next) {
     }
 }
 
-router.delete('/api/posts/:id', checkLogin, async (req, res) => {
-    
-    const postToDelete = await PostModel.findOne({ _id: req.params.id });
+// ENDPOINTS
 
-    if (req.session.access === "admin" || req.session.username === postToDelete.user) {
-        await postToDelete.remove();
-        res.status(200).json("Post was deleted.");
-      } else {
-        res.status(403).json("You can only delete your own posts.");
-    }
+router.get('/api/posts', async (req, res) => {
+    const posts = await PostModel.find({});
+    res.status(200).json(posts);
 });
 
-router.put('/api/posts/:id', checkLogin, async (req, res) => {
-
-    const postToEdit = await PostModel.findOne({ _id: req.params.id });
-    console.log(postToEdit)
-
-    if (req.session.access === "admin" || req.session.username === postToEdit.user) {
-        postToEdit.update({"message": req.body.message});
-        res.status(200).json("Post was edited: " + req.body.message);
-      } else {
-        res.status(403).json("You can only edit your own posts.");
-    }
+router.get('/api/posts/adminaccess', checkAdminAccess, async (req, res) => {
+    const posts = await PostModel.find({});
+    res.status(200).json(posts);
 });
 
 router.post('/api/posts', checkLogin, async (req, res) => {
@@ -64,7 +43,41 @@ router.post('/api/posts', checkLogin, async (req, res) => {
     }
 
     const post = await PostModel.create(postToSave);
-    res.status(201).json("Post was created.");
+    res.status(201).json("Post was created: " + post.message);
+});
+
+router.put('/api/posts/:id', checkLogin, async (req, res) => {
+
+    const postToEdit = await PostModel.findOne({ _id: req.params.id });
+
+    if (postToEdit) {
+        if (req.session.access === "admin" || req.session.username === postToEdit.user) {
+            await PostModel.updateOne({ _id: req.params.id }, {message: req.body.message})
+            res.status(200).json("Post was edited: " + req.body.message);
+          } else {
+            res.status(403).json("You can only edit your own posts.");
+        }
+    }
+    else {
+        res.status(401).json("Post does not exist.");
+    }
+});
+
+router.delete('/api/posts/:id', checkLogin, async (req, res) => {
+    
+    const postToDelete = await PostModel.findOne({ _id: req.params.id });
+
+    if(postToDelete) {
+        if (req.session.access === "admin" || req.session.username === postToDelete.user) {
+            await postToDelete.remove();
+            res.status(200).json("Post was deleted.");
+          } else {
+            res.status(403).json("You can only delete your own posts.");
+        }
+    }
+    else {
+        res.status(401).json("Post does not exist.");
+    }
 });
 
 module.exports = router;
